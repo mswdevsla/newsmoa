cee4acd650a48b5a287fc8da73fac3a5de6b2430
msw123123 <msw123123@gmail.com>
2017-03-25 21:55:38 +0900

    newsmoa project

--- newsproject/home/views.py
+++ newsproject/home/views.py
@@ -16,11 +16,19 @@ def home(request):
         i = i+1
 
     if request.user.is_authenticated:
-        news_customs = NewsCustom.objects.filter(user_info__user=request.user)
-    else:
-        news_customs = None
+        news_contents = []
+        news_customs = NewsCustom.objects.filter(user_info__user=request.user).order_by('priority')
+        for news_custom in news_customs:
+            i = 1
+            feed = feedparser.parse(news_custom.news_content.xml_address)
+            for item in feed.entries:
+                item['news_company'] = news_custom.news_content.get_company_name
+                news_contents.append(item)
+                i = i+1
+                if i > news_custom.how_many:
+                    break
 
     return render(request, 'home.html', context=({
-        'content': content,
-        'news_customs': news_customs
+        'news_customs': news_customs,
+        'news_contents': news_contents
     }))
--- newsproject/news_component/views.py
+++ newsproject/news_component/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from newsproject.news_component.models import NewsContent
+from newsproject.user.models import NewsCustom
 from django.contrib.auth.decorators import login_required
 
 # @login_required
 def config(request):
+    from django.http import HttpResponse
     contents = NewsContent.objects.all()
     companys = contents.distinct('news_company')
     sections = contents.distinct('news_section')
+    if request.method == 'POST':
+        for i in range(1, 11):
+            if not request.POST.get('section' + str(i)) == None and not request.POST.get('company' + str(i)) == None and not request.POST.get('how_many' + str(i)) == None:
+                news_section = request.POST.get('section' + str(i))
+                news_company = request.POST.get('company' + str(i))
+                how_many = request.POST.get('how_many' + str(i))
+
+                try:
+                    news_content = NewsContent.objects.get(news_section=news_section, news_company=news_company)
+                except:
+                    return HttpResponse('<script>alert("해당하는 신문사의 섹션이 존재하지 않습니다"); history.back();</script>')
+
+                try:
+                    news_custom = NewsCustom.objects.get(user_info__user=request.user, priority=i)
+                    news_custom.news_section = news_section
+                    news_custom.news_company = news_company
+                    news_custom.how_many = how_many
+                    news_custom.save()
+
+                except:
+                    NewsCustom.objects.create(user_info__user=request.user, news_content=news_content, how_many=how_many,
+                                              priority=i)
+
     return render(request, 'news_component/config.html', context={
         'contents': contents,
         'companys': companys,
