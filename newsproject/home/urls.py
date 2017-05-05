8812d960d2663df177b82c36de30ac1ceab8c230
msw123123 <sw.moon@specupad.com>
2017-05-05 13:02:46 +0900

    commit 2

--- newsproject/home/urls.py
+++ newsproject/home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^user/', include('newsproject.user.urls')),
     url(r'^news_component/', include('newsproject.news_component.urls')),
+    url(r'^news_board/', include('newsproject.news_board.urls')),
 ]
